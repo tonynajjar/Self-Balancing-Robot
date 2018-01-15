d955cf2681c45c20de14c7ed9247862e3ecd3158
tonynajjar <tony.alnajjar@lau.edu>
2018-01-15 20:31:03 +0200

    Added Rapsberry Pi Getch

---
+++ Raspberry Pi/getch.py
@@ -0,0 +1,27 @@
+import sys, tty, termios
+import serial
+
+ser = serial.Serial(port='/dev/serial0', baudrate=115200,
+                            parity=serial.PARITY_NONE,
+                            stopbits=serial.STOPBITS_ONE,
+                            bytesize=serial.EIGHTBITS,
+                            timeout=1)
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+while True:
+    char = getch()
+    print char
+    if char == 'p':
+        break
+        
+    ser.write(char)
+
+
